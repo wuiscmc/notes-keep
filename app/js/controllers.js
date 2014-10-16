fa76123366b982ba1c2208e78971dfea18dc5575
Carlos Mateos <wuiscmc@gmail.com>
2014-10-16 14:43:43 +0200

    Added edition of cards

---
+++ app/js/controllers.js
@@ -0,0 +1,81 @@
+/**
+* CardControllers Module
+*
+* Description
+*/
+
+var cardControllers = angular.module('CardControllers', ['underscore']);
+
+cardControllers.controller('CardListCtrl', ['$scope', '$http', '_', function($scope, $http, _){
+
+  $scope.cards = [];
+  $scope.newCard = {content: '', date: ''}
+  $scope.adding = false;
+  $scope.editing = false;
+
+  initialize();
+
+  $scope.addCard = function() {
+    $scope.adding = true;
+  }
+
+  $scope.cancelAdd = function() {
+    finishAdd();
+  }
+
+  $scope.saveCard = function() {
+    $scope.cards.push($scope.newCard);
+    finishAdd();
+  }
+
+  $scope.removeCard = function(id) {
+    $scope.cards = _.reject($scope.cards, function(card){ return card.id == id;});
+  }
+
+  $scope.editCard = function(card) {
+    $scope.lockedCard = angular.copy(card);
+    $scope.editing = true;
+  }
+
+  $scope.updateCard = function() { 
+    var editCardIndex = _.findIndex($scope.cards, function(card){
+      return card.id == $scope.lockedCard.id;
+    });
+    $scope.cards[editCardIndex] = $scope.lockedCard;
+    finishEditing();
+  }
+
+  $scope.cancelEdit = function() {
+    finishEditing();
+  }
+
+  /*
+  * initialization stuff
+  */
+  function initialize(){
+    loadCards();
+  }
+
+  function loadCards(){
+    $http.get('cards.json').
+      success(function(data){
+        $scope.cards = _.map(angular.fromJson(data), function(card){
+          card.date = new Date(card.date);
+          return card;
+        });
+      });
+  }
+
+  function finishAdd() {
+    $scope.adding = false;
+    $scope.newCard = {content: '', date: ''};
+  };
+
+  function finishEditing() {
+    $scope.editing = false;
+    $scope.lockedCard = null;
+  }
+
+}]);
+
+
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,12 +5,12 @@ describe('controllers', function() {
 
   beforeEach(module('notekeep'));
 
-  describe('CardList', function(){
+  describe('CardListCtrl', function(){
     var scope, ctrl, $httpBackend;
 
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
       scope = $rootScope.$new();
-      ctrl = $controller('CardList', {$scope: scope});
+      ctrl = $controller('CardListCtrl', {$scope: scope});
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('cards.json').
         respond([
@@ -18,29 +18,92 @@ describe('controllers', function() {
           {id: 2, content: 'sample2', date: '12/12/2014'}]);
     }));
 
-    it('should load the cards from the json', function() {
-      $httpBackend.flush();
-      expect(scope.cards.length).toBe(2);
+    describe('card addition', function(){
+      it('gets locked until the card is added', function(){
+        expect(scope.adding).toBe(false);
+        scope.addCard();
+        expect(scope.adding).toBe(true);
+        scope.saveCard();
+        expect(scope.adding).toBe(false);
+       });
+
+      it('lets you add a card', function(){
+        scope.cards = [];
+        scope.addCard();
+        scope.newCard = newCard();
+        scope.saveCard();
+        expect(scope.newCard.content).toBe('');
+        expect(scope.cards.length).toBe(1);
+        expect(scope.cards[0].content).toBe('testy');
+      })
+
+      it('unlocks when cancelled', function(){
+        expect(scope.adding).toBe(false);
+        scope.addCard();
+        expect(scope.adding).toBe(true);
+        scope.cancelAdd();
+        expect(scope.adding).toBe(false);
+      });
     });
 
-    it('allows to add new cards', function(){
-      $httpBackend.flush();
-      expect(scope.cards.length).toBe(2);
-      scope.newCard = {id: 3, content: 'sample3', date: '12/12/2015'};
-      scope.addCard();
-      expect(scope.cards.length).toBe(3);
-      expect(scope.cards[2].content).toBe('sample3');
-      expect(scope.newCard.content).toBe('');
+    describe('card edition', function(){
+      beforeEach(function(){
+        scope.cards = [mockCard()];
+        expect(scope.editing).toBe(false);
+        scope.editCard(scope.cards[0]);
+      });
+
+      it('should lock the edit form', function(){
+        expect(scope.editing).toBe(true);
+        scope.updateCard();
+        expect(scope.editing).toBe(false);
+      });
+
+      it('should update a card', function(){
+        expect(scope.lockedCard.content).toBe(scope.cards[0].content);
+        var newContent = "updated content for this shit";
+        scope.lockedCard.content = newContent;
+        scope.updateCard();
+        expect(scope.cards[0].content).toBe(newContent);
+      });
+
+      it('doesnt update anything when cancelled', function(){
+        var newContent = "updated content for this shit";
+        scope.lockedCard.content = newContent;
+        scope.cancelEdit();
+        expect(scope.cards[0].content).not.toBe(newContent);
+      });
+
+
     });
 
-    it('allows to remove cards', function(){
-      $httpBackend.flush();
-      expect(scope.cards.length).toBe(2);
-      scope.removeCard(1);
-      expect(scope.cards.length).toBe(1);
-      expect(scope.cards[0].id).toBe(2);
+    describe('card list', function(){
+      it('should load the cards from the json', function() {
+        $httpBackend.flush();
+        expect(scope.cards.length).toBe(2);
+      });
+
+      it('allows to remove cards', function(){
+        scope.cards = [mockCard(1), mockCard(2)];
+        expect(scope.cards.length).toBe(2);
+        scope.removeCard(1);
+        expect(scope.cards.length).toBe(1);
+        expect(scope.cards[0].id).toBe(2);
+      });
     });
+
 
   });
+
+  function newCard() {
+    return {content: 'testy', date: '12/12/2025'};
+  };
+
+  function mockCard(id) {
+    var fakeid = id || parseInt((Math.random() * 10) + 10);
+    var content = newCard();
+    content['id'] = fakeid;
+    return content;
+  }
 
 });
